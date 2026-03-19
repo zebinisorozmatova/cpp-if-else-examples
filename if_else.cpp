16ff5369a63793b58eb1bacce47213c1f08b80e4
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-19 23:47:15 +0500

    Create if_else.cpp

---
+++ if_else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+
+    if (a > 5) {
+        cout << "Katta";
+    } else {
+        cout << "Kichik";
+    }
+
+    return 0;
+}
