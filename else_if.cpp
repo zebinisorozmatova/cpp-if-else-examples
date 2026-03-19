6d1fff99cfcf99ab194cf33ee0fa29f88fda327b
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-19 23:47:57 +0500

    Create else_if.cpp

---
+++ else_if.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 0;
+
+    if (a > 0) {
+        cout << "Musbat";
+    } else if (a < 0) {
+        cout << "Manfiy";
+    } else {
+        cout << "Nol";
+    }
+}
